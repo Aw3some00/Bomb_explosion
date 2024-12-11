83fb97f435bdb64312f630bc8c73f1ffa53a76b0
afandieva <emilyafandieva123@gmail.com>
2024-12-11 22:31:56 +0400

    project

--- bomb.cpp
+++ bomb.cpp
@@ -1,90 +1,77 @@
 #include <iostream>
 #include <vector>
-#include <math.h>
+#include <queue>
+#include <cmath>
+#include <ctime>
+#include "bomb.h"
 
 namespace checkers {
     bool count_checker(double const percent) {
-        if(percent > 100000000) {
+        if (percent > 100000000) {
             throw std::invalid_argument("count of bombs is too high");
             return false;
-        }
-        else if(percent < 0) {
+        } else if (percent < 0) {
             throw std::invalid_argument("count of bombs is too small");
             return false;
-        }
-        else if(percent == 0) {
-            std::cout<<"there is no bomb on the field\n";
+        } else if (percent == 0) {
+            std::cout << "there is no bomb on the field\n";
+            return false;
+        } else if (percent == 100000000) {
+            std::cout << "all the bombs make a max row\n";
             return false;
         }
-        else if(percent == 100000000) {
-            std::cout<<"all the bombs make a max row\n";
-            return false;
-        }
-
+        return true;
     }
-}
+
     bool r_checker(double const radius) {
-        if(radius < 0) {
+        if (radius < 0) {
             throw std::invalid_argument("radius is too small");
             return false;
-        }
-        else if(radius >=10000) {
+        } else if (radius >= 10000) {
             throw std::invalid_argument("radius is too high");
             return false;
-        }
-        else if(radius == 0) {
-            std::cout<<"max explosion lenght is 0";
+        } else if (radius == 0) {
+            std::cout << "max explosion length is 0";
+            return false;
+        } else if (radius == 10000) {
+            std::cout << "max explosion length is all the bombs";
             return false;
         }
-        else if(radius == 10000) {
-            std::cout<<"max explosion lenght it is all the bombs";
-            return false;
-        }
+        return true;
     }
-    namespace b {
-        struct Bomb {
+}
 
-            double x;
-            double y;
-            bool is_exploded=false;
-        };
+namespace b {
 
-        bool is_in_radius(Bomb a,Bomb b,double r) {
-            double ds= (a.x-b.x)*(a.x-b.x)+(a.y-b.y)*(a.y-b.y);
-            if(ds<=r*r) {
-                return true;
-            }
-            else {
-                return false;
-            }
-        }
-
-        std::vector<Bomb> generateBombs(int rows, int cols, int ones_count) {
-            std::vector<Bomb> bombs;
-
-
-            std::srand(std::time(nullptr));
-
-            while (bombs.size() < ones_count) {
-
-                double x = static_cast<double>(std::rand()) / RAND_MAX * rows;
-                double y = static_cast<double>(std::rand()) / RAND_MAX * cols;
-
-                bombs.push_back({x, y});
-            }
-
-            return bombs;
-        }
-
-
-        void printBombs(const std::vector<Bomb>& bombs) {
-            for (const auto& bomb : bombs) {
-                std::cout << "Bomb at (" << bomb.x << ", " << bomb.y << ")\n";
-            }
-        }
+    bool is_in_radius(Bomb a, Bomb b, double r) {
+        double distance_squared = (a.x - b.x) * (a.x - b.x) + (a.y - b.y) * (a.y - b.y);
+        return distance_squared <= r * r;
     }
 
 
+    int simulate_chain_reaction(const std::vector<Bomb>& bombs, int start_index, double r) {
+        std::vector<bool> visited(bombs.size(), false);
+        std::queue<int> q;
+        int detonated_count = 0;
 
 
+        q.push(start_index);
+        visited[start_index] = true;
+
+        while (!q.empty()) {
+            int current = q.front();
+            q.pop();
+            detonated_count++;
+
+
+            for (int i = 0; i < bombs.size(); ++i) {
+                if (!visited[i] && is_in_radius(bombs[current], bombs[i], r)) {
+                    q.push(i);
+                    visited[i] = true;
+                }
+            }
+        }
+        return detonated_count;
+    }
+}
 
--- bomb.h
+++ bomb.h
@@ -1,22 +1,27 @@
 #ifndef BOMB_H
 #define BOMB_H
+
 #include <iostream>
 #include <vector>
 
 namespace checkers {
     bool count_checker(double const percent);
     bool r_checker(double const r);
-
 }
-namespace b{
+
+namespace b {
     struct Bomb {
         double x;
         double y;
         bool is_exploded;
     };
-    bool is_in_radius(Bomb a,Bomb b,double r);
+
+    bool is_in_radius(Bomb a, Bomb b, double r);
     std::vector<Bomb> generateBombs(int rows, int cols, int ones_count);
     void printBombs(const std::vector<Bomb>& bombs);
 
+    // Функция для симуляции детонации
+    int simulate_chain_reaction(const std::vector<Bomb>& bombs, int start_index, double r);
 }
-#endif //BOMB_H
+
+#endif // BOMB_H
