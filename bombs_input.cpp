047ba5bc9f5e64bdb13a2a970743c38585a13c62
Aw3some00 <kotnarkot962@gmail.com>
2024-12-26 09:05:00 +0400

    исправлены баги

--- bombs_input.cpp
+++ bombs_input.cpp
@@ -4,7 +4,43 @@
 #include "bomb.h"
 #include "bombs_input.h"
 #include <thread>
+#include <iomanip>
+#include <cctype>
+bool is_int(const std::string& str) {
+    if (str.empty()) return false;
 
+    size_t start = 0;
+    if (str[0] == '-') {
+         return false;
+    }
+
+    for (size_t i = start; i < str.size(); ++i) {
+        if (!isdigit(str[i])) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+bool is_valid_input(const std::string& input) {
+    size_t dot_count = 0;
+    for (char c: input) {
+        if (c == '.') {
+            dot_count++;
+        } else if (!isdigit(c) && c != '-') {
+            return false;
+        }
+    }
+    return dot_count <= 1;
+}
+
+double strrtodbl(const std::string& input) {
+    if (!is_valid_input(input)) {
+        throw std::invalid_argument("Некорректный формат числа");
+    }
+    return std::stod(input);
+}
 
 bool is_duplicate(const std::vector<b::Bomb>& bombs, const b::Bomb& new_bomb) {
     for (const auto& bomb : bombs) {
@@ -37,60 +73,103 @@ void Animation() {
 
 
 void collect_info(std::vector<b::Bomb>& bombs) {
-    double count;
+    std::string count;
     std::cout << "Введите количество бомб(Let's make some explosions!): ";
     std::cin >> count;
-
-    if (std::cin.fail()) {
-        throw std::invalid_argument("Введите число для количества бомб");
+    if(count.size()>12){
+        throw std::invalid_argument("слишком много цифр для работы с ними ");
     }
-
-    if (!checkers::count_checker(count)) {
-        std::cerr << "Неверное количество бомб\n";
-        throw std::invalid_argument("Неверное количество бомб");
-    }
-
-    b::Bomb bomb;
-    for (int i = 0; i < count; i++) {
-        std::cout << "Введите координаты и радиус бомбы под номером " << i + 1 << ":\n";
-        std::cout << "Координата X: ";
-        std::cin >> bomb.x;
-        if (std::cin.fail()) {
-            throw std::invalid_argument("Введите число для координаты X или координаты Y");
-        }
-
-        std::cout << "Координата Y: ";
-        std::cin >> bomb.y;
-        if (std::cin.fail()) {
-            throw std::invalid_argument("Введите число для координаты X или координаты Y");
-        }
-
-        std::cout << "Радиус: ";
-        std::cin >> bomb.radius;
-        if (std::cin.fail()) {
-            throw std::invalid_argument("Введите число для радиуса");
-        }
-
-        if (checkers::r_checker(bomb.radius) &&
-            checkers::borders_check(bomb.x) &&
-            checkers::borders_check(bomb.y)) {
-
-
-            if (is_duplicate(bombs, bomb)) {
-                std::cout << "Ошибка! Бомба с такими координатами уже существует!\n";
-                i--;
-                continue;
+    else
+        if (is_int(count)) {
+            if (std::cin.fail()) {
+                throw std::invalid_argument("Введите число для количества бомб");
             }
 
-            bombs.push_back(bomb);
-            std::cout << "Бомба добавлена! Координаты: (" << bomb.x << ", " << bomb.y << "), радиус: "
-                      << bomb.radius << std::endl;
+            if (!checkers::count_checker(std::stoi(count))) {
+                throw std::invalid_argument("Неверное количество бомб");
+            }
+
+            b::Bomb bomb;
+            for (int i = 0; i < std::stoi(count); i++) {
+                std::cout << "Введите координаты и радиус бомбы под номером " << i + 1 << ":\n";
+                std::string input;
+                while (true) {
+                    if (input.size() < 15) {
+                        std::cout << "Координата X: ";
+                        std::cin >> input;
+                        try {
+                            bomb.x = strrtodbl(input);
+                            break;
+                        } catch (const std::invalid_argument &e) {
+                            std::cout << "Ошибка: " << e.what() << "\n";
+                            std::cout << "Попробуйте снова \n";
+                        }
+                    } else {
+                        throw std::invalid_argument(
+                                "cлишком большое,слишком близкое к нулю или слишком маленькое число для X");
+                    }
+                }
+                while (true) {
+                    std::cout << "Координата Y: ";
+                    std::cin >> input;
+                    if (input.size() < 15) {
+                        try {
+                            bomb.y = strrtodbl(input);
+                            break;
+                        } catch (const std::invalid_argument &e) {
+                            std::cout << "Ошибка: " << e.what() << "\n";
+                            std::cout << "Попробуйте снова.\n";
+                        }
+                    } else {
+                        throw std::invalid_argument(
+                                "cлишком большое,слишком близкое к нулю или слишком маленькое число для Y");
+                    }
+                }
+
+                while (true) {
+                    std::cout << "Радиус: ";
+                    std::cin >> input;
+                    if (input.size() < 15) {
+                        try {
+                            bomb.radius = strrtodbl(input);
+                            break;
+                        } catch (const std::invalid_argument &e) {
+                            std::cout << "Ошибка: " << e.what() << "\n";
+                            std::cout << "Попробуйте снова.\n";
+                        }
+                    } else {
+                        throw std::invalid_argument(
+                                "cлишком большое,слишком близкое к нулю или слишком маленькое число для радиуса");
+                    }
+
+                }
+
+                if (checkers::r_checker(bomb.radius) &&
+                    checkers::borders_check(bomb.x) &&
+                    checkers::borders_check(bomb.y)) {
+
+
+                    if (is_duplicate(bombs, bomb)) {
+                        std::cout << "Ошибка! Бомба с такими координатами уже существует!\n";
+                        i--;
+                        continue;
+                    }
+
+                    bombs.push_back(bomb);
+                    std::cout << "Бомба добавлена! Координаты: (" << std::setprecision(15) << bomb.x << ", "
+                              << std::setprecision(15) << bomb.y << ") радиус: "
+                              << std::setprecision(15) << bomb.radius << "\n";
+                } else {
+                    std::cout << "Ошибка в данных бомбы под номером " << i + 1
+                              << ". Пожалуйста, введите данные снова.\n";
+                    i--;
+                }
+            }
         } else {
-            std::cout << "Ошибка в данных бомбы под номером " << i + 1 << ". Пожалуйста, введите данные снова.\n";
-            i--;
+            throw std::invalid_argument("Неправильные данные");
         }
     }
-}
+
 
 
 
@@ -99,8 +178,8 @@ void collect_info(std::vector<b::Bomb>& bombs) {
 void print_result(std::vector<b::Bomb>& bombs, std::vector<int>& max_indexes, double max_area) {
     std::cout << "\nВведенные вами данные:\n";
     for (size_t i = 0; i < bombs.size(); ++i) {
-        std::cout << "Бомба " << i + 1 << " на (" << bombs[i].x << ", " << bombs[i].y
-                  << ") с радиусом " << bombs[i].radius << "\n";
+        std::cout << "Бомба " << i + 1 << " на (" <<std::setprecision(15)<<bombs[i].x<<", "<< std::setprecision(15)<<bombs[i].y
+                  << ") с радиусом " <<std::setprecision(15)<< bombs[i].radius << "\n";
     }
     std::cout << "\n";
 
@@ -114,5 +193,5 @@ void print_result(std::vector<b::Bomb>& bombs, std::vector<int>& max_indexes, do
     }
     std::cout << "\n";
 
-    std::cout << "Площадь (взорванная) " << max_area << "\n";
+    std::cout << "Площадь (взорванная) " <<std::setprecision(15)<<max_area<< "\n";
 }
--- bombs_input.h
+++ bombs_input.h
@@ -9,6 +9,8 @@ bool is_duplicate(const std::vector<b::Bomb>& bombs, const b::Bomb& new_bomb);
 void Animation() ;
 void collect_info(std::vector<b::Bomb>& bombs);
 void print_result(std::vector<b::Bomb>& bombs, std::vector<int>& max_indexes, double max_area);
-
+bool is_valid_input(const std::string& input);
+double strrtodbl(const std::string& input);
+bool is_int(const std::string& input);
 
 #endif //BOMB_INPUT_H
